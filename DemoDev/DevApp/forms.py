c0ed4c32a25502b99cefcd5c37d38fb920275c48
C T PRASANTH <prasanthmonish@gmail.com>
2020-03-11 01:03:26 +0530

    Multiplt thumbnails
    Multiplt thumbnails

--- DemoDev/DevApp/forms.py
+++ DemoDev/DevApp/forms.py
@@ -3,7 +3,8 @@ from django.forms import ModelForm
 from .models import Inventory
 class InventoryForm(ModelForm):
     image = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True,'class' : 'upload'}))
-
+    brand = forms.CharField(widget=forms.TextInput(attrs={'class':'brand','id':'brand'}))
+    price = forms.CharField(widget=forms.TextInput(attrs={'class':'price','id':'price'}))
 
     class Meta:
         model=Inventory
