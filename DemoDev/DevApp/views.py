b25e2e8544b3813e5aa66cdb1a10179c7fd42777
C T PRASANTH <prasanthmonish@gmail.com>
2020-03-30 02:30:48 +0530

    Update views.py

--- DemoDev/DevApp/views.py
+++ DemoDev/DevApp/views.py
@@ -7,6 +7,7 @@ from .forms import InventoryForm,FlipForm,OrderForm
 from django.forms import modelformset_factory
 import os.path
 from django.core.files.base import ContentFile
+import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -49,6 +50,7 @@ def register(request):
         else:
             messages.error(request,"Password not matching")
             return redirect('register')
+	os.system("touch /var/www/www_salehound_net_wsgi.py")
     return render(request,'DevApp/register.html/')
 
 
